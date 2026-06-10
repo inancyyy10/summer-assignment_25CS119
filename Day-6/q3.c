2986e21266f5260b496e543dab249537e47f85af
inancyyy10 <kaushiknancy108@gmail.com>
2026-06-10 14:18:00 +0530

    Create Day-6/q3.c

---
+++ Day-6/q3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    while (n > 0) {
+        n = n & (n - 1); 
+        count++;
+    }
+
+    printf("Set bits: %d", count);
+    return 0;
+}
