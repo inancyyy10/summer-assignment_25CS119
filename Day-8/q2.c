02d3c1d45590455230a15ec4f032df491c6c5a00
inancyyy10 <kaushiknancy108@gmail.com>
2026-06-14 19:46:36 +0530

    Update q2.c

--- Day-8/q2.c
+++ Day-8/q2.c
@@ -1,12 +1,25 @@
-#include <stdio.h>
+/*Write a program to Print number triangle.
+1
+12
+123
+1234
+12345*/
 
-int main() {
-    int i, j;
-    for (i = 1; i <= 5; i++) {
-        for (j = 1; j <= i; j++) {
+#include <stdio.h>
+int main() 
+{
+int i, j, r;
+printf("Enter number of rows: ");
+scanf("%d", &r);
+
+  for(i = 1; i <= r; i++) 
+    {
+     for(j = 1; j <= i; j++)
+         {
             printf("%d", j);
-        }
-        printf("\n");
+         }
+      printf("\n");
     }
-    return 0;
+return 0;
+
 }
