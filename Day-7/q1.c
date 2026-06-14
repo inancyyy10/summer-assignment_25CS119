e45159abe10355db689df7c452df228d80631f12
inancyyy10 <kaushiknancy108@gmail.com>
2026-06-14 19:43:24 +0530

    Update q1.c

--- Day-7/q1.c
+++ Day-7/q1.c
@@ -1,12 +1,41 @@
+//Write a program to Recursive factorial.
+
 #include <stdio.h>
 
-int factorial(int n) {
-    if (n <= 1) return 1;
-    return n * factorial(n - 1);
+int f(int n) 
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * f(n - 1);
 }
 
-int main() {
-    int num = 5;
-    printf("Factorial of %d is %d\n", num, factorial(num));
+int main() {#include <stdio.h>
+
+int f(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * f(n - 1);
+}
+
+int main() 
+  {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factorial = %d\n", f(n));
+
+    return 0;
+   }
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factorial = %d\n", f(n));
+
     return 0;
 }
