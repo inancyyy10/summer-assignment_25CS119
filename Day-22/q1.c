1cd00c57ff4f261fbb25a86fe41f9d00c7198f41
inancyyy10 <kaushiknancy108@gmail.com>
2026-06-24 11:07:22 +0530

    Create Day-22/q1.c

---
+++ Day-22/q1.c
@@ -0,0 +1,27 @@
+//Write a program to Check palindrome string.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    int i, len,result = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", s);
+
+    len = strlen(s);
+
+    for(i = 0; i < len / 2; i++) {
+        if(s[i] != s[len - 1 - i]) {
+            result = 0;
+            break;
+        }
+    }
+
+    if(result)
+        printf("Palindrome String");
+    else
+        printf("Not a Palindrome String");
+
+    return 0;
+}
