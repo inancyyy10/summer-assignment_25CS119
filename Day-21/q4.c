8177180115fee3c8a88bac202f0c868abd625981
inancyyy10 <kaushiknancy108@gmail.com>
2026-06-24 11:03:09 +0530

    Create Day-21/q4.c

---
+++ Day-21/q4.c
@@ -0,0 +1,18 @@
+//Write a program to Convert lowercase to uppercase.
+#include <stdio.h>
+int main()
+{
+    char l;
+    printf("Enter the lowercase letter :");
+    scanf("%c",&l);
+    if(l>='a'&& l<='z')
+    {
+        l=l-32;
+        printf("uppercase letter : %c",l);
+    }
+    else 
+    {
+        printf("not a lowercase character");
+    }
+    return 0;
+}
